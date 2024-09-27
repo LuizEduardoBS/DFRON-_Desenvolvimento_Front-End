9871f1f6ccc2525c70256d413ee3b8799ffb7d09
123 <123@gmail.com>
2024-09-26 22:18:48 -0300

    At

--- my-library-project/library-api/server.js
+++ my-library-project/library-api/server.js
@@ -7,11 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// mongoose.connect('mongodb+srv://luized:Luiz123@cluster0.u7srm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// })
-
 // Conexão ao MongoDB
 mongoose.connect('mongodb+srv://luized:Luiz123@cluster0.u7srm.mongodb.net/library', {
     useNewUrlParser: true,
