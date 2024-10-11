653bf6c07dda7de8d7a9f9c3f934bcbbf3f49d7a
Luiz Eduardo <luiz131998@gmail.com>
2024-10-10 21:53:13 -0300

    At

---
+++ AUTENTIFICACAO-USER-my-library-project/library-api/routes/authRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express'); // Importa o express
+const router = express.Router(); // Cria um roteador do express
+const authController = require('../controllers/authController')
+
+// Rota para registrar novos usuários
+router.post('/login', authController.login);
+
+module.exports = router; // Exporta o roteador para uso no app.js
