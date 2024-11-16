22e476aa5e13416f903081425de062c65cb25015
Luiz Eduardo <luiz131998@gmail.com>
2024-11-16 19:46:03 -0300

    At foto carregada no carrinho e reservas

--- Z_Biblioteca/frontend/src/views/UserReservationsPage.vue
+++ Z_Biblioteca/frontend/src/views/UserReservationsPage.vue
@@ -24,7 +24,7 @@ import { RouterLink, RouterView } from 'vue-router'
               <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
             </svg>
           </div>
-          <img src="" alt="">
+          <img :src="formatImagePath(book.bookId.coverImage)" alt="">
           <div>
             <span>{{ book.bookId.title || 'Nome do livro não disponível' }}</span>
           </div>
@@ -154,6 +154,9 @@ export default {
       });
 
   },
+  formatImagePath(path) {
+    return `http://localhost:3000/${path.replace(/\\/g, '/')}`;
+  }
 
 },
 mounted() {
